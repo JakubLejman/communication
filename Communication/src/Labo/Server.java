3922a06f95d06fe17f37f97b72dea33bc30820e5
JakubLejman <kubalejman@wp.pl>
2023-01-21 16:50:54 +0100

    message

--- Communication/src/Labo/Server.java
+++ Communication/src/Labo/Server.java
@@ -8,53 +8,48 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(4444);
+        } catch (IOException e) {
+            System.err.println("SERVER: Could not listen on port: 4444, " + e);
+            System.exit(1);
+        }
+        System.out.println("SERVER: Server connection opened on port 4444.");
+        while (true) {
+            Socket clientSocket = null;
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("SERVER: Accept failed: 4444, " + e);
+                System.exit(1);
+            }
+            System.out.println("SERVER: Accepted client connecion on port 4444.");
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                System.out.println("SERVER: IN and OUT streams opened. Starting receiving data...");
 
-		while (true) {
-			ServerSocket serverSocket = null;
-			try {
-				serverSocket = new ServerSocket(4444);
-			} catch (IOException e) {
-				System.err.println("SERVER: Could not listen on port: 4444, " + e);
-				System.exit(1);
-			}
-			System.out.println("SERVER: Server connection opened on port 4444.");
+                out.println("Witam na serwerze. Czekam na dane.");
+                out.flush();
 
-			Socket clientSocket = null;
-			try {
-				// after this method server stops and waits for client connection
-				clientSocket = serverSocket.accept();
-				serverSocket.close();
-			} catch (IOException e) {
-				System.err.println("SERVER: Accept failed: 4444, " + e);
-				System.exit(1);
-			}
-			System.out.println("SERVER: Accepted client connecion on port 4444.");
-
-			try {
-				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-				System.out.println("SERVER: IN and OUT streams opened. Starting receiving data...");
-
-				out.println("Witam na serwerze. Czekam na dane.");
-				out.flush();
-
-				String inputLine, outputLine;
-				ChangeOrder handler = new ChangeOrder();
-				while ((inputLine = in.readLine()) != null) {
-					outputLine = handler.processMessage(inputLine);
-					out.println(outputLine);
-					out.flush();
-					if (inputLine.equals("koniec"))
-						break;
-				}
-				System.out.println("SERVER: Ending sequence received. Closing streams and sockets.");
-				out.close();
-				in.close();
-				clientSocket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+                String inputLine, outputLine;
+                ChangeOrder handler = new ChangeOrder();
+                while ((inputLine = in.readLine()) != null) {
+                    outputLine = handler.processMessage(inputLine);
+                    out.println(outputLine);
+                    out.flush();
+                    if (inputLine.equals("koniec"))
+                        break;
+                }
+                System.out.println("SERVER: Ending sequence received. Closing streams and sockets.");
+                out.close();
+                in.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
